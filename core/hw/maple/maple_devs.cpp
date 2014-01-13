53fcb163aa8ab0f75c5b2f08af22cfc162f3de43
hooby3dfx <hooby3d@gmail.com>
2014-01-13 17:02:22 -0500

    initial dox
    mic device figured out?
    
    i have satisfied seaman.
    its just constantly polling for data.
    next step: pipe in some audio!
    
    almost working
    
    crashes - need to figure out jni thread crap
    
    oh snap gets further!
    
    jni local reference table overflow
    
    AWWWW YEA
    
    SOMEONE TRY THIS

--- core/hw/maple/maple_devs.cpp
+++ core/hw/maple/maple_devs.cpp
@@ -4,6 +4,8 @@
 #include "maple_devs.h"
 #include "maple_cfg.h"
 #include <time.h>
+#include <android/log.h>
+#include <jni.h>
 
 #include "deps/zlib/zlib.h"
 
@@ -18,6 +20,7 @@ const char* maple_sega_mic_name = "MicDevice for Dreameye";
 const char* maple_sega_brand = "Produced By or Under License From SEGA ENTERPRISES,LTD.";
 
 #define HAS_VMU
+#define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG, "maple_devs", __VA_ARGS__)
 
 enum MapleFunctionID
 {
@@ -655,10 +658,15 @@ struct maple_microphone: maple_base
 {
 	virtual u32 dma(u32 cmd)
 	{
-		printf("maple_microphone::dma Called 0x%X;Command %d\n",this->maple_port,cmd);
+		//printf("maple_microphone::dma Called 0x%X;Command %d\n",this->maple_port,cmd);
+		//LOGD("maple_microphone::dma Called 0x%X;Command %d\n",this->maple_port,cmd);
+
 		switch (cmd)
 		{
 		case MDC_DeviceRequest:
+			LOGD("maple_microphone::dma MDC_DeviceRequest");
+			//this was copied from the controller case with just the id and name replaced!
+
 			//caps
 			//4
 			w32(MFID_4_Mic);
@@ -689,9 +697,10 @@ struct maple_microphone: maple_base
 
 			return MDRS_DeviceStatus;
 
-			//controller condition
 		case MDCF_GetCondition:
 			{
+				//this was copied from the controller case with just the id replaced!
+
 				//PlainJoystickState pjs;
 				//config->GetInput(&pjs);
 				//caps
@@ -722,14 +731,78 @@ struct maple_microphone: maple_base
 				w8(0x80);
 			}
 
-		return MDRS_DataTransfer;
-		
+			return MDRS_DataTransfer;
+
 		case MDC_DeviceReset:
-		
-		return MDRS_DeviceReply;
+			//uhhh do nothing?
+			LOGD("maple_microphone::dma MDC_DeviceReset");
+			return MDRS_DeviceReply;
+
+		case MDCF_MICControl:
+		{
+			//LOGD("maple_microphone::dma handling MDCF_MICControl %d\n",cmd);
+			//MONEY
+			u32 function=r32();
+			//LOGD("maple_microphone::dma MDCF_MICControl function (1st word) %#010x\n", function);
+			//LOGD("maple_microphone::dma MDCF_MICControl words: %d\n", dma_count_in);
+
+			switch(function)
+			{
+			case MFID_4_Mic:
+			{
+				//MAGIC HERE
+				//http://dcemulation.org/phpBB/viewtopic.php?f=34&t=69600
+				// <3 <3 BlueCrab <3 <3
+				/*
+				2nd word               What it does:
+				0x0000??03          Sets the amplifier gain, ?? can be from 00 to 1F
+									0x0f = default
+				0x00008002          Enables recording
+				0x00000001          Returns sampled data while recording is enabled
+									While not enabled, returns status of the mic.
+				0x00000002          Disables recording
+				 *
+				 */
+				u32 secondword=r32();
+				//LOGD("maple_microphone::dma MDCF_MICControl subcommand (2nd word) %#010x\n", subcommand);
+
+				u32 subcommand = secondword & 0xFF; //just get last byte for now, deal with params later
+
+				//LOGD("maple_microphone::dma MDCF_MICControl (3rd word) %#010x\n", r32());
+				//LOGD("maple_microphone::dma MDCF_MICControl (4th word) %#010x\n", r32());
+				switch(subcommand)
+				{
+				case 0x01:
+				{
+					LOGD("maple_microphone::dma MDCF_MICControl someone wants some data!");
+					//maximum size of a Maple Bus packet is 256 words (1024 bytes)
+					//maybe i should start with 512 (same as vmu)
+					u8* micdata=(u8*)malloc(512);
+					get_mic_data(micdata);
+					wptr(micdata, 512);
+					
+					return MDRS_DataTransfer;
+				}
+				case 0x02:
+					LOGD("maple_microphone::dma MDCF_MICControl toggle recording!");
+					//this is where i should start recording...
+
+					return MDRS_DeviceReply;
+				case 0x03:
+					return MDRS_DeviceReply;
+				default:
+					LOGD("maple_microphone::dma UNHANDLED secondword %#010x\n",secondword);
+					break;
+				}
+			}
+			default:
+				LOGD("maple_microphone::dma UNHANDLED function %#010x\n",function);
+				break;
+			}
+		}
 
 		default:
-			printf("maple_microphone::dma UNHANDLED MAPLE COMMAND %d\n",cmd);
+			LOGD("maple_microphone::dma UNHANDLED MAPLE COMMAND %d\n",cmd);
 			return MDRE_UnknownFunction;
 		}
 	}	

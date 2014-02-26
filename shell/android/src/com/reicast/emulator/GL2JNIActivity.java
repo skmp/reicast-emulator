872da3d3326fd02a77ab64ac19675a8834d08271
TwistedUmbrella <twistedumbrella@gmail.com>
2014-02-26 17:46:46 -0500

    Toggle sound independently, Implement Fastforward instead

--- shell/android/src/com/reicast/emulator/GL2JNIActivity.java
+++ shell/android/src/com/reicast/emulator/GL2JNIActivity.java
@@ -183,7 +183,8 @@ public class GL2JNIActivity extends Activity {
 			fileName = Uri.decode(intent.getData().toString());
 
 		// Create the actual GLES view
-		mView = new GL2JNIView(getApplication(), config, fileName, false, prefs.getInt("depth_render", 24), 0, false);
+		mView = new GL2JNIView(getApplication(), config, fileName, false,
+				prefs.getInt("depth_render", 24), 0, false);
 		setContentView(mView);
 		
 		String menu_spec;

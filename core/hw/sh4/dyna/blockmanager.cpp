016b7559955645a8bb530ad986cbd2898cd94d0a
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2015-08-03 16:48:47 +0200

    core/hw/sh4/dyna/blockmanager.cpp: Fix a bunch of format string warnings
    These messages were fixed:
    ../../core/hw/sh4/dyna/blockmanager.cpp:125:42: warning: format specifies type
          'unsigned int' but the argument has type 'void *' [-Wformat]
                    printf("bm_GetBlock(%08X) failed ..\n",dynarec_code);
                                        ~~~~               ^~~~~~~~~~~~
    ../../core/hw/sh4/dyna/blockmanager.cpp:152:66: warning: format specifies type
          'unsigned int' but the argument has type 'DynarecCodeEntryPtr'
          (aka 'void (*)()') [-Wformat]
      ...%08X %08X %08X\n", (*blkmap.find(blk))->addr,(*blkmap.find(blk))->code,b...
         ~~~~                                         ^~~~~~~~~~~~~~~~~~~~~~~~~
    ../../core/hw/sh4/dyna/blockmanager.cpp:152:102: warning: format specifies type
          'unsigned int' but the argument has type 'DynarecCodeEntryPtr'
          (aka 'void (*)()') [-Wformat]
      ...%08X\n", (*blkmap.find(blk))->addr,(*blkmap.find(blk))->code,blk->addr,blk->code);
         ~~~~                                                                   ^~~~~~~~~
    ../../core/hw/sh4/dyna/blockmanager.cpp:452:92: warning: format specifies type
          'unsigned int' but the argument has type 'DynarecCodeEntryPtr'
          (aka 'void (*)()') [-Wformat]
      ...%08X:%d:%d:%d\n",all_blocks[i]->BlockType,all_blocks[i]->addr,all_blocks[i]->code...
         ~~~~                                                          ^~~~~~~~~~~~~~~~~~~
    ../../core/hw/sh4/dyna/blockmanager.cpp:454:34: warning: format specifies type
          'int' but the argument has type 'size_t' (aka 'unsigned long') [-Wformat]
                                    fprintf(f,"\top: %d:%d:%s\n",j,all_block...
                                                     ~~          ^
                                                     %lu
    ../../core/hw/sh4/dyna/blockmanager.cpp:485:17: warning: flag '0' results in
          undefined behavior with 'p' conversion specifier [-Wformat]
                    fprintf(out,"%08p %d %08X\n",all_blocks[i]->code,all_blo...
                                 ~^~~
    ../../core/hw/sh4/dyna/blockmanager.cpp:515:25: warning: format specifies type
          'unsigned int' but the argument has type 'DynarecCodeEntryPtr'
          (aka 'void (*)()') [-Wformat]
                            all_blocks[i]->addr, all_blocks[i]->code,all_blo...
                                                 ^~~~~~~~~~~~~~~~~~~
    ../../core/hw/sh4/dyna/blockmanager.cpp:525:40: warning: invalid conversion
          specifier 'b' [-Wformat-invalid-specifier]
            printf(" >-< %.2f%% covered in top 1% blocks\n",sel_hops/total_hops);
                                                ~~^
    ../../core/hw/sh4/dyna/blockmanager.cpp:531:25: warning: format specifies type
          'unsigned int' but the argument has type 'DynarecCodeEntryPtr'
          (aka 'void (*)()') [-Wformat]
                            all_blocks[i]->addr, all_blocks[i]->code,all_blo...
                                                 ^~~~~~~~~~~~~~~~~~~
    ../../core/hw/sh4/dyna/blockmanager.cpp:638:29: warning: format specifies type
          'unsigned int' but the argument has type 'DynarecCodeEntryPtr'
          (aka 'void (*)()') [-Wformat]
                            fprintf(f,"code: %08X\n",blk->code);
                                             ~~~~    ^~~~~~~~~
    ../../core/hw/sh4/dyna/blockmanager.cpp:648:33: warning: format specifies type
          'int' but the argument has type 'size_type' (aka 'unsigned long')
          [-Wformat]
                            fprintf(f,"il_opcodes: %d\n",blk->oplist.size());
                                                   ~~    ^~~~~~~~~~~~~~~~~~
                                                   %lu

--- core/hw/sh4/dyna/blockmanager.cpp
+++ core/hw/sh4/dyna/blockmanager.cpp
@@ -122,7 +122,7 @@ RuntimeBlockInfo* bm_GetBlock(void* dynarec_code)
 	}
 	else
 	{
-		printf("bm_GetBlock(%08X) failed ..\n",dynarec_code);
+		printf("bm_GetBlock(%p) failed ..\n",dynarec_code);
 		return 0;
 	}
 }
@@ -149,7 +149,7 @@ void bm_AddBlock(RuntimeBlockInfo* blk)
 	all_blocks.push_back(blk);
 	if (blkmap.find(blk)!=blkmap.end())
 	{
-		printf("DUP: %08X %08X %08X %08X\n", (*blkmap.find(blk))->addr,(*blkmap.find(blk))->code,blk->addr,blk->code);
+		printf("DUP: %08X %p %08X %p\n", (*blkmap.find(blk))->addr,(*blkmap.find(blk))->code,blk->addr,blk->code);
 		verify(false);
 	}
 	blkmap.insert(blk);
@@ -449,9 +449,9 @@ void bm_WriteBlockMap(const string& file)
 		printf("Writing block map !\n");
 		for (size_t i=0; i<all_blocks.size(); i++)
 		{
-			fprintf(f,"block: %d:%08X:%08X:%d:%d:%d\n",all_blocks[i]->BlockType,all_blocks[i]->addr,all_blocks[i]->code,all_blocks[i]->host_code_size,all_blocks[i]->guest_cycles,all_blocks[i]->guest_opcodes);
+			fprintf(f,"block: %d:%08X:%p:%d:%d:%d\n",all_blocks[i]->BlockType,all_blocks[i]->addr,all_blocks[i]->code,all_blocks[i]->host_code_size,all_blocks[i]->guest_cycles,all_blocks[i]->guest_opcodes);
 			for(size_t j=0;j<all_blocks[i]->oplist.size();j++)
-				fprintf(f,"\top: %d:%d:%s\n",j,all_blocks[i]->oplist[j].guest_offs,all_blocks[i]->oplist[j].dissasm().c_str());
+				fprintf(f,"\top: %lu:%d:%s\n",j,all_blocks[i]->oplist[j].guest_offs,all_blocks[i]->oplist[j].dissasm().c_str());
 		}
 		fclose(f);
 		printf("Finished writing block map\n");
@@ -482,7 +482,7 @@ void sh4_jitsym(FILE* out)
 {
 	for (size_t i=0; i<all_blocks.size(); i++)
 	{
-		fprintf(out,"%08p %d %08X\n",all_blocks[i]->code,all_blocks[i]->host_code_size,all_blocks[i]->addr);
+		fprintf(out,"%p %d %08X\n",all_blocks[i]->code,all_blocks[i]->host_code_size,all_blocks[i]->addr);
 	}
 }
 
@@ -511,7 +511,7 @@ void bm_PrintTopBlocks()
 	double sel_hops=0;
 	for (size_t i=0;i<(all_blocks.size()/100);i++)
 	{
-		printf("Block %08X: %06X, r: %d (c: %d, s: %d, h: %d) (r: %.2f%%, c: %.2f%%, h: %.2f%%)\n",
+		printf("Block %08X: %p, r: %d (c: %d, s: %d, h: %d) (r: %.2f%%, c: %.2f%%, h: %.2f%%)\n",
 			all_blocks[i]->addr, all_blocks[i]->code,all_blocks[i]->runs,
 			all_blocks[i]->guest_cycles,all_blocks[i]->guest_opcodes,all_blocks[i]->host_opcodes,
 
@@ -522,12 +522,12 @@ void bm_PrintTopBlocks()
 		sel_hops+=all_blocks[i]->host_opcodes*all_blocks[i]->runs;
 	}
 
-	printf(" >-< %.2f%% covered in top 1% blocks\n",sel_hops/total_hops);
+	printf(" >-< %.2f%% covered in top 1%% blocks\n",sel_hops/total_hops);
 
 	size_t i;
 	for (i=all_blocks.size()/100;sel_hops/total_hops<50;i++)
 	{
-		printf("Block %08X: %06X, r: %d (c: %d, s: %d, h: %d) (r: %.2f%%, c: %.2f%%, h: %.2f%%)\n",
+		printf("Block %08X: %p, r: %d (c: %d, s: %d, h: %d) (r: %.2f%%, c: %.2f%%, h: %.2f%%)\n",
 			all_blocks[i]->addr, all_blocks[i]->code,all_blocks[i]->runs,
 			all_blocks[i]->guest_cycles,all_blocks[i]->guest_opcodes,all_blocks[i]->host_opcodes,
 
@@ -635,7 +635,7 @@ void print_blocks()
 			fprintf(f,"addr: %08X\n",blk->addr);
 			fprintf(f,"hash: %s\n",blk->hash());
 			fprintf(f,"hash_rloc: %s\n",blk->hash(false,true));
-			fprintf(f,"code: %08X\n",blk->code);
+			fprintf(f,"code: %p\n",blk->code);
 			fprintf(f,"runs: %d\n",blk->runs);
 			fprintf(f,"BlockType: %d\n",blk->BlockType);
 			fprintf(f,"NextBlock: %08X\n",blk->NextBlock);
@@ -645,7 +645,7 @@ void print_blocks()
 			fprintf(f,"guest_cycles: %d\n",blk->guest_cycles);
 			fprintf(f,"guest_opcodes: %d\n",blk->guest_opcodes);
 			fprintf(f,"host_opcodes: %d\n",blk->host_opcodes);
-			fprintf(f,"il_opcodes: %d\n",blk->oplist.size());
+			fprintf(f,"il_opcodes: %lu\n",blk->oplist.size());
 
 			u32 hcode=0;
 			s32 gcode=-1;

0f6258b96d379c39815e1bba36bab0179032d98a
twinaphex <libretro@gmail.com>
2015-10-12 23:51:49 +0200

    Start making libpng dependency unnecessary

--- core/rend/gles/gles.cpp
+++ core/rend/gles/gles.cpp
@@ -624,8 +624,6 @@ bool CompilePipelineShader(	PipelineShader* s)
 	return glIsProgram(s->program)==GL_TRUE;
 }
 
-GLuint osd_tex;
-
 bool gl_create_resources()
 {
 
@@ -706,11 +704,6 @@ bool gl_create_resources()
 			return false;
 	}
 	#endif
-
-#if 0
-	int w, h;
-	osd_tex=loadPNG(get_readonly_data_path("/data/buttons.png"),w,h);
-#endif
 
 	return true;
 }
@@ -1384,157 +1377,5 @@ struct glesrend : Renderer
 		return gl_GetTexture(tsp, tcw);
 	}
 };
-
-
-#include "deps/libpng/png.h"
-
-FILE* pngfile;
-
-void png_cstd_read(png_structp png_ptr, png_bytep data, png_size_t length)
-{
-	fread(data,1, length,pngfile);
-}
-
-GLuint loadPNG(const string& fname, int &width, int &height)
-{
-	const char* filename=fname.c_str();
-	FILE* file = fopen(filename, "rb");
-	pngfile=file;
-
-	if (!file)
-	{
-		printf("Error opening %s\n", filename);
-		return TEXTURE_LOAD_ERROR;
-	}
-
-	//header for testing if it is a png
-	png_byte header[8];
-
-	//read the header
-	fread(header,1,8,file);
-
-	//test if png
-	int is_png = !png_sig_cmp(header, 0, 8);
-	if (!is_png)
-	{
-		fclose(file);
-		printf("Not a PNG file : %s", filename);
-		return TEXTURE_LOAD_ERROR;
-	}
-
-	//create png struct
-	png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL,
-		NULL, NULL);
-	if (!png_ptr)
-	{
-		fclose(file);
-		printf("Unable to create PNG struct : %s", filename);
-		return (TEXTURE_LOAD_ERROR);
-	}
-
-	//create png info struct
-	png_infop info_ptr = png_create_info_struct(png_ptr);
-	if (!info_ptr)
-	{
-		png_destroy_read_struct(&png_ptr, (png_infopp) NULL, (png_infopp) NULL);
-		printf("Unable to create PNG info : %s", filename);
-		fclose(file);
-		return (TEXTURE_LOAD_ERROR);
-	}
-
-	//create png info struct
-	png_infop end_info = png_create_info_struct(png_ptr);
-	if (!end_info)
-	{
-		png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp) NULL);
-		printf("Unable to create PNG end info : %s", filename);
-		fclose(file);
-		return (TEXTURE_LOAD_ERROR);
-	}
-
-	//png error stuff, not sure libpng man suggests this.
-	if (setjmp(png_jmpbuf(png_ptr)))
-	{
-		fclose(file);
-		printf("Error during setjmp : %s", filename);
-		png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
-		return (TEXTURE_LOAD_ERROR);
-	}
-
-	//init png reading
-	//png_init_io(png_ptr, fp);
-	png_set_read_fn(png_ptr, NULL, png_cstd_read);
-
-	//let libpng know you already read the first 8 bytes
-	png_set_sig_bytes(png_ptr, 8);
-
-	// read all the info up to the image data
-	png_read_info(png_ptr, info_ptr);
-
-	//variables to pass to get info
-	int bit_depth, color_type;
-	png_uint_32 twidth, theight;
-
-	// get info about png
-	png_get_IHDR(png_ptr, info_ptr, &twidth, &theight, &bit_depth, &color_type,
-		NULL, NULL, NULL);
-
-	//update width and height based on png info
-	width = twidth;
-	height = theight;
-
-	// Update the png info struct.
-	png_read_update_info(png_ptr, info_ptr);
-
-	// Row size in bytes.
-	int rowbytes = png_get_rowbytes(png_ptr, info_ptr);
-
-	// Allocate the image_data as a big block, to be given to opengl
-	png_byte *image_data = new png_byte[rowbytes * height];
-	if (!image_data)
-	{
-		//clean up memory and close stuff
-		png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
-		printf("Unable to allocate image_data while loading %s ", filename);
-		fclose(file);
-		return TEXTURE_LOAD_ERROR;
-	}
-
-	//row_pointers is for pointing to image_data for reading the png with libpng
-	png_bytep *row_pointers = new png_bytep[height];
-	if (!row_pointers)
-	{
-		//clean up memory and close stuff
-		png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
-		delete[] image_data;
-		printf("Unable to allocate row_pointer while loading %s ", filename);
-		fclose(file);
-		return TEXTURE_LOAD_ERROR;
-	}
-
-	// set the individual row_pointers to point at the correct offsets of image_data
-	for (int i = 0; i < height; ++i)
-		row_pointers[height - 1 - i] = image_data + i * rowbytes;
-
-	//read the png into image_data through row_pointers
-	png_read_image(png_ptr, row_pointers);
-
-	//Now generate the OpenGL texture object
-	GLuint texture;
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA,
-		GL_UNSIGNED_BYTE, (GLvoid*) image_data);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-
-	//clean up memory and close stuff
-	png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
-	delete[] image_data;
-	delete[] row_pointers;
-	fclose(file);
-
-	return texture;
-}
-
 
 Renderer* rend_GLES2() { return new glesrend(); }

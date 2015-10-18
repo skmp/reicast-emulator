7f9f1dd0aeaace248803aa8e8552c8c50670b264
twinaphex <libretro@gmail.com>
2015-10-18 02:57:38 +0200

    Cleanups

--- core/reios/reios_elf.cpp
+++ core/reios/reios_elf.cpp
@@ -4,28 +4,28 @@
 
 #include "hw/sh4/sh4_mem.h"
 
-bool reios_loadElf(const string& elf) {
-
+bool reios_loadElf(const string& elf)
+{
+   void *elfFile;
+	int i;
+	bool phys = false;
+   size_t size;
 	FILE* f = fopen(elf.c_str(), "rb");
-	if (!f) {
+	if (!f)
 		return false;
-	}
+
 	fseek(f, 0, SEEK_END);
-	size_t size = ftell(f);
+	size = ftell(f);
 
-	if (size > 16 * 1024 * 1024) {
+	if (size > 16 * 1024 * 1024)
 		return false;
-	}
 
-	void* elfFile = malloc(size);
+	elfFile = malloc(size);
 	memset(elfFile, 0, size);
 
 	fseek(f, 0, SEEK_SET);
 	fread(elfFile, 1, size, f);
 	fclose(f);
-
-	int i;
-	bool phys = false;
 
 
 	if (elf_checkFile(elfFile) != 0) {
@@ -33,16 +33,15 @@ bool reios_loadElf(const string& elf) {
 		return false;
 	}
 
-	for (i = 0; i < elf_getNumProgramHeaders(elfFile); i++) {
+	for (i = 0; i < elf_getNumProgramHeaders(elfFile); i++)
+   {
 		/* Load that section */
 		uint64_t dest, src;
 		size_t len;
-		if (phys) {
+		if (phys)
 			dest = elf_getProgramHeaderPaddr(elfFile, i);
-		}
-		else {
+		else
 			dest = elf_getProgramHeaderVaddr(elfFile, i);
-		}
 		len = elf_getProgramHeaderFileSize(elfFile, i);
 		src = (uint64_t)(uintptr_t)elfFile + elf_getProgramHeaderOffset(elfFile, i);
 		

bc0bfb6c1c2e23527ad08e0305e4a73ae344d621
twinaphex <libretro@gmail.com>
2015-10-18 02:32:16 +0200

    Remove USES_HOMEDIR

--- core/linux-dist/main.cpp
+++ core/linux-dist/main.cpp
@@ -19,10 +19,6 @@
 	#include "linux-dist/x11.h"
 #endif
 
-#if defined(USES_HOMEDIR)
-	#include <sys/stat.h>
-#endif
-
 #if defined(USE_EVDEV)
 	#include "linux-dist/evdev.h"
 #endif
@@ -165,84 +161,12 @@ void dc_run();
 
 string find_user_config_dir()
 {
-	#ifdef USES_HOMEDIR
-		struct stat info;
-		string home = "";
-		if(getenv("HOME") != NULL)
-		{
-			// Support for the legacy config dir at "$HOME/.reicast"
-			string legacy_home = (string)getenv("HOME") + "/.reicast";
-			if((stat(legacy_home.c_str(), &info) == 0) && (info.st_mode & S_IFDIR))
-			{
-				// "$HOME/.reicast" already exists, let's use it!
-				return legacy_home;
-			}
-
-			/* If $XDG_CONFIG_HOME is not set, we're supposed to use "$HOME/.config" instead.
-			 * Consult the XDG Base Directory Specification for details:
-			 *   http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html#variables
-			 */
-			home = (string)getenv("HOME") + "/.config/reicast";
-		}
-		if(getenv("XDG_CONFIG_HOME") != NULL)
-		{
-			// If XDG_CONFIG_HOME is set explicitly, we'll use that instead of $HOME/.config
-			home = (string)getenv("XDG_CONFIG_HOME") + "/reicast";
-		}
-
-		if(!home.empty())
-		{
-			if((stat(home.c_str(), &info) != 0) || !(info.st_mode & S_IFDIR))
-			{
-				// If the directory doesn't exist yet, create it!
-				mkdir(home.c_str(), 0755);
-			}
-			return home;
-		}
-	#endif
-
 	// Unable to detect config dir, use the current folder
 	return ".";
 }
 
 string find_user_data_dir()
 {
-	#ifdef USES_HOMEDIR
-		struct stat info;
-		string data = "";
-		if(getenv("HOME") != NULL)
-		{
-			// Support for the legacy config dir at "$HOME/.reicast"
-			string legacy_data = (string)getenv("HOME") + "/.reicast";
-			if((stat(legacy_data.c_str(), &info) == 0) && (info.st_mode & S_IFDIR))
-			{
-				// "$HOME/.reicast" already exists, let's use it!
-				return legacy_data;
-			}
-
-			/* If $XDG_DATA_HOME is not set, we're supposed to use "$HOME/.local/share" instead.
-			 * Consult the XDG Base Directory Specification for details:
-			 *   http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html#variables
-			 */
-			data = (string)getenv("HOME") + "/.local/share/reicast";
-		}
-		if(getenv("XDG_DATA_HOME") != NULL)
-		{
-			// If XDG_DATA_HOME is set explicitly, we'll use that instead of $HOME/.config
-			data = (string)getenv("XDG_DATA_HOME") + "/reicast";
-		}
-		
-		if(!data.empty())
-		{
-			if((stat(data.c_str(), &info) != 0) || !(info.st_mode & S_IFDIR))
-			{
-				// If the directory doesn't exist yet, create it!
-				mkdir(data.c_str(), 0755);
-			}
-			return data;
-		}
-	#endif
-
 	// Unable to detect config dir, use the current folder
 	return ".";
 }
@@ -299,8 +223,7 @@ std::vector<string> find_system_data_dirs()
 	return dirs;
 }
 
-
-int main(int argc, wchar* argv[])
+static void retro_init(int argc, wchar *argv[] )
 {
 	/* Set directories */
 	set_user_config_dir(find_user_config_dir());
@@ -308,14 +231,10 @@ int main(int argc, wchar* argv[])
 	std::vector<string> dirs;
 	dirs = find_system_config_dirs();
 	for(unsigned int i = 0; i < dirs.size(); i++)
-	{
 		add_system_data_dir(dirs[i]);
-	}
 	dirs = find_system_data_dirs();
 	for(unsigned int i = 0; i < dirs.size(); i++)
-	{
 		add_system_data_dir(dirs[i]);
-	}
 	printf("Config dir is: %s\n", get_writable_config_path("/").c_str());
 	printf("Data dir is:   %s\n", get_writable_data_path("/").c_str());
 
@@ -330,6 +249,12 @@ int main(int argc, wchar* argv[])
 #if FEAT_HAS_NIXPROF
    install_prof_handler(0);
 #endif
+}
+
+int main(int argc, wchar* argv[])
+{
+   retro_init(argc, argv);
+
    dc_run();
 
 	return 0;

5373bc5ca794180f4fbe0bd57a56d347aa88edc9
twinaphex <libretro@gmail.com>
2015-12-19 04:19:45 +0100

    AudioStream - simplify code

--- core/oslib/audiostream.cpp
+++ core/oslib/audiostream.cpp
@@ -3,41 +3,34 @@
 #include "audiostream.h"
 #include "../libretro/libretro.h"
 
-struct SoundFrame { s16 l;s16 r; };
 #define SAMPLE_COUNT 512
 
-SoundFrame RingBuffer[SAMPLE_COUNT];
-const u32 RingBufferByteSize = sizeof(RingBuffer);
-const u32 RingBufferSampleCount = SAMPLE_COUNT;
-
-volatile u32 WritePtr;  //last WRITEN sample
-volatile u32 ReadPtr;   //next sample to read
+struct SoundFrame
+{
+   s16 l;
+   s16 r;
+};
 
 extern retro_audio_sample_batch_t audio_batch_cb;
 
-bool RegisterAudioBackend(void)
-{
-	return true;
-}
+bool RegisterAudioBackend(void) { return true; }
 
-void RegisterAllAudioBackends() {
-}
-
-u32 PushAudio(void* frame, u32 amt, bool wait)
-{
-   audio_batch_cb((const int16_t*)frame, amt);
-	return amt;
-}
+void RegisterAllAudioBackends() { }
 
 void WriteSample(s16 r, s16 l)
 {
-	const u32 ptr=(WritePtr+1)%RingBufferSampleCount;
+   static SoundFrame RingBuffer[SAMPLE_COUNT];
+   static const u32 RingBufferByteSize = sizeof(RingBuffer);
+   static const u32 RingBufferSampleCount = SAMPLE_COUNT;
+   static volatile u32 WritePtr;  //last WRITEN sample
+   static volatile u32 ReadPtr;   //next sample to read
+	const u32 ptr = (WritePtr+1)%RingBufferSampleCount;
 	RingBuffer[ptr].r=r;
 	RingBuffer[ptr].l=l;
 	WritePtr=ptr;
 
 	if (WritePtr==(SAMPLE_COUNT-1))
-		PushAudio(RingBuffer,SAMPLE_COUNT,settings.aica.LimitFPS);
+      audio_batch_cb((const int16_t*)RingBuffer, SAMPLE_COUNT);
 }
 
 void InitAudio()

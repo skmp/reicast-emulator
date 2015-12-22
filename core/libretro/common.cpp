45142805ae5d063aad32110d5fd5cd60c7f2ecff
Twinaphex <libretro@gmail.com>
2015-12-22 23:04:05 +0100

    Get rid of platform-specific PC grabbing

--- core/libretro/common.cpp
+++ core/libretro/common.cpp
@@ -305,20 +305,7 @@ static void fault_handler (int sn, siginfo_t * si, void *segfault_ctx)
 
 #ifdef LOG_SIGHANDLER
 
-#if HOST_CPU==CPU_X64
-#ifdef __linux__
-   ucontext_t* uctx=(ucontext_t*)&ctx;
-   printf("mprot hit @ ptr 0x%08X @@ code: %08X, %d\n",si->si_addr,uctx->uc_mcontext.gregs[REG_RIP],dyna_cde);
-#elif defined(__MACH__)
-   ucontext_t* uctx=(ucontext_t*)&ctx;
-   printf("mprot hit @ ptr 0x%08X @@ code: %08X, %d\n",si->si_addr,uctx->uc_mcontext->__ss.__rip,dyna_cde);
-#endif
-#elif HOST_CPU==CPU_ARM
-#ifdef __linux__
-   ucontext_t* uctx=(ucontext_t*)&ctx;
-   printf("mprot hit @ ptr 0x%08X @@ code: %08X, %d\n",si->si_addr,uctx->uc_mcontext.arm_pc,dyna_cde);
-#endif
-#endif
+printf("mprot hit @ ptr 0x%08X @@ code: %08X, %d\n", ctx.pc, dyna_cde);
 
 #endif
 

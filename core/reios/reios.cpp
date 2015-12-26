0657ed1024895b7b22627cc50e30ea2cc68ee7dc
twinaphex <libretro@gmail.com>
2015-12-26 15:48:46 +0100

    Ability to read IP.bin now

--- core/reios/reios.cpp
+++ core/reios/reios.cpp
@@ -98,6 +98,8 @@ bool reios_locate_bootfile(const char* bootfile="1ST_READ.BIN") {
 }
 
 char reios_bootfile[32];
+char ip_bin[256];
+
 const char* reios_locate_ip() {
 
 	if (libGDR_GetDiscType() == GdRom) {
@@ -111,6 +113,13 @@ const char* reios_locate_ip() {
 		base_fad = (ses[3] << 16) | (ses[4] << 8) | (ses[5] << 0);
 		descrambl = true;
 	}
+	
+	libGDR_ReadSector(GetMemPtr(0x8c008000, 0), base_fad, 256, 2048);
+
+	memset(ip_bin, 0, sizeof(ip_bin));
+	memcpy(ip_bin, GetMemPtr(0x8c008000, 0), 256);
+
+	printf("reios: IP.bin is '%s'\n", ip_bin);
 
 	printf("reios: loading ip.bin from FAD: %d\n", base_fad);
 

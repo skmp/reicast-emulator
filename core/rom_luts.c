e3102cd9a400f24f41839f6a87db9f7caef3f586
twinaphex <libretro@gmail.com>
2015-12-30 22:27:12 +0100

    We don't need per-game hacks anymore for these two

--- core/rom_luts.c
+++ core/rom_luts.c
@@ -8,7 +8,5 @@ struct game_type
 static struct game_type lut_games[] = 
 {
    { "MK-51049  ",  1, -1 },                /* Marvel Vs Capcom 2 */
-   { "MK-51035  ", -1, -1 },                /* Capcom Vs SNK 2 */
    { "T1203N    ", -1,  1 },                /* Street Fighter Alpha 3 */
-   { "T1213N    ", -1, -1 },                /* Street Fighter III -3rd Strike */
 };

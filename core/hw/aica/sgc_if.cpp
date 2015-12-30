ec91acf2a49622e7f3363fbc6978892f34332620
twinaphex <libretro@gmail.com>
2015-12-30 18:41:35 +0100

    Rename some variables to be closer to MESS/MAME implementation

--- core/hw/aica/sgc_if.cpp
+++ core/hw/aica/sgc_if.cpp
@@ -85,22 +85,22 @@ s32 tl_lut[256 + 768];	//xx.15 format. >=255 is muted
 
 /* Envelope time in milliseconds */
 
-double AEG_Attack_Time[]=
+double ARTimes[64]=
 {
-	-1,-1,8100.0,6900.0,6000.0,4800.0,4000.0,3400.0,3000.0,2400.0,2000.0,1700.0,1500.0,
+	-1, -1 ,8100.0,6900.0,6000.0,4800.0,4000.0,3400.0,3000.0,2400.0,2000.0,1700.0,1500.0,
 	1200.0,1000.0,860.0,760.0,600.0,500.0,430.0,380.0,300.0,250.0,220.0,190.0,150.0,130.0,110.0,95.0,
 	76.0,63.0,55.0,47.0,38.0,31.0,27.0,24.0,19.0,15.0,13.0,12.0,9.4,7.9,6.8,6.0,4.7,3.8,3.4,3.0,2.4,
 	2.0,1.8,1.6,1.3,1.1,0.93,0.85,0.65,0.53,0.44,0.40,0.35,0.0,0.0
 };
-double AEG_DSR_Time[]=
-{	-1,-1,118200.0,101300.0,88600.0,70900.0,59100.0,50700.0,44300.0,35500.0,29600.0,25300.0,22200.0,17700.0,
+double DRTimes[64]=
+{	-1, -1,118200.0,101300.0,88600.0,70900.0,59100.0,50700.0,44300.0,35500.0,29600.0,25300.0,22200.0,17700.0,
 	14800.0,12700.0,11100.0,8900.0,7400.0,6300.0,5500.0,4400.0,3700.0,3200.0,2800.0,2200.0,1800.0,1600.0,1400.0,1100.0,
 	920.0,790.0,690.0,550.0,460.0,390.0,340.0,270.0,230.0,200.0,170.0,140.0,110.0,98.0,85.0,68.0,57.0,49.0,43.0,34.0,
 	28.0,25.0,22.0,18.0,14.0,12.0,11.0,8.5,7.1,6.1,5.4,4.3,3.6,3.1
 };
 //Steps per sample
-u32 AEG_ATT_SPS[MAX_CHANNELS];
-u32 AEG_DSR_SPS[MAX_CHANNELS];
+u32 ARTABLE[64];
+u32 DRTABLE[64];
 
 const char* stream_names[]=
 {
@@ -519,7 +519,7 @@ static u32 SlotAEG_EffRate(struct ChannelEx *ch, u32 re)
 {
    s32 rv = ch->ccd->KRS + (ch->ccd->FNS>>9) + re*2;
 
-   if (ch->ccd->KRS==0xF)
+   if (ch->ccd->KRS == 0xF)
       rv -= 0xF;
 
    if (ch->ccd->OCT&8)
@@ -537,11 +537,11 @@ static u32 SlotAEG_EffRate(struct ChannelEx *ch, u32 re)
 //D2R,D1R,AR,DL,RR,KRS, [OCT,FNS] for now
 static void SlotUpdateAEG(struct ChannelEx *ch)
 {
-   ch->AEG.AttackRate   = AEG_ATT_SPS[SlotAEG_EffRate(ch, ch->ccd->AR)];
-   ch->AEG.Decay1Rate   = AEG_DSR_SPS[SlotAEG_EffRate(ch, ch->ccd->D1R)];
+   ch->AEG.AttackRate   = ARTABLE[SlotAEG_EffRate(ch, ch->ccd->AR)];
+   ch->AEG.Decay1Rate   = DRTABLE[SlotAEG_EffRate(ch, ch->ccd->D1R)];
    ch->AEG.Decay2Value  = ch->ccd->DL<<5;
-   ch->AEG.Decay2Rate   = AEG_DSR_SPS[SlotAEG_EffRate(ch, ch->ccd->D2R)];
-   ch->AEG.ReleaseRate  = AEG_DSR_SPS[SlotAEG_EffRate(ch, ch->ccd->RR)];
+   ch->AEG.Decay2Rate   = DRTABLE[SlotAEG_EffRate(ch, ch->ccd->D2R)];
+   ch->AEG.ReleaseRate  = DRTABLE[SlotAEG_EffRate(ch, ch->ccd->RR)];
 }
 
 /* OCT,FNS */
@@ -1090,8 +1090,8 @@ void sgc_Init(void)
 
    for (int i = 0; i < MAX_CHANNELS; i++)
    {
-      AEG_ATT_SPS[i]=Calc_EG_Steps(AEG_Attack_Time[i]);
-      AEG_DSR_SPS[i]=Calc_EG_Steps(AEG_DSR_Time[i]);
+      ARTABLE[i]     = Calc_EG_Steps(ARTimes[i]);
+      DRTABLE[i]     = Calc_EG_Steps(DRTimes[i]);
    }
 
    for (int i = 0; i < MAX_CHANNELS; i++)

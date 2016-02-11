b0b43042457667e9ca67e1318b698fc89d935c15
twinaphex <libretro@gmail.com>
2016-02-11 20:20:34 +0100

    Update glsm

--- core/libretro-common/glsm/glsm.c
+++ core/libretro-common/glsm/glsm.c
@@ -441,7 +441,7 @@ void rglBindFramebuffer(GLenum target, GLuint framebuffer)
    glsm_ctl(GLSM_CTL_IMM_VBO_DRAW, NULL);
    if (!glsm_ctl(GLSM_CTL_IS_FRAMEBUFFER_LOCKED, NULL))
    {
-      glBindFramebuffer(RARCH_GL_FRAMEBUFFER, framebuffer);
+      glBindFramebuffer(target, framebuffer);
       gl_state.framebuf = framebuffer;
    }
 }

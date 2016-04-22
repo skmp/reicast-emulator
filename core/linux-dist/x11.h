4f0b1e9731bb20a311be050d5bc2103a840c627a
7oxicshadow <7oxicshadow@googlemail.com>
2016-04-22 23:11:39 +0100

    Allow Linux build to close cleanly *Add the ability to stop the render thread *Add the ability to stop the ngen thread *Add the ability to detect the window close event *Add gl_context_destroy function *Close any open controllers *Allows the nvmem to save

--- core/linux-dist/x11.h
+++ core/linux-dist/x11.h
@@ -3,5 +3,6 @@
 extern void* x11_glc;
 extern void input_x11_init();
 extern void input_x11_handle();
+extern void event_x11_handle();
 extern void x11_window_create();
 extern void x11_window_set_text(const char* text);

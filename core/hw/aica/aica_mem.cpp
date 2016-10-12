59d62285c7bb23db2ede053a5f8afa8a672776e3
twinaphex <libretro@gmail.com>
2016-10-12 17:39:41 +0200

    aicamem.cpp - get rid of templated functions

--- core/hw/aica/aica_mem.cpp
+++ core/hw/aica/aica_mem.cpp
@@ -9,110 +9,119 @@ u8 aica_reg[0x8000];
 //00800000~008027FF @CHANNEL_DATA 
 //00802800~00802FFF @COMMON_DATA 
 //00803000~00807FFF @DSP_DATA 
-template<u32 sz>
-static u32 ReadReg(u32 addr)
+
+static u32 ReadReg1(u32 addr)
 {
 	if (addr<0x2800)
-	{
-		ReadMemArrRet(aica_reg,addr,sz);
-	}
-	if (addr < 0x2818)
-	{
-		if (sz==1)
-		{
-			ReadCommonReg(addr,true);
-         return aica_reg[addr];
-		}
-		else
-		{
-			ReadCommonReg(addr,false);
-			//ReadCommonReg8(addr+1);
-         return *(u16*)&aica_reg[addr];
-		}
-	}
+		ReadMemArrRet(aica_reg,addr,1);
 
-	ReadMemArrRet(aica_reg,addr,sz);
+	if (addr < 0x2818)
+   {
+      ReadCommonReg(addr,true);
+      return aica_reg[addr];
+   }
+
+	ReadMemArrRet(aica_reg,addr,1);
 }
 
-template<u32 sz>
-static void WriteReg(u32 addr,u32 data)
+static u32 ReadReg2(u32 addr)
 {
-	if (addr < 0x2000)
-	{
-		//Channel data
-		u32 chan=addr>>7;
-		u32 reg=addr&0x7F;
-		if (sz==1)
-		{
-         aica_reg[addr]=(u8)data;
-			WriteChannelReg8(chan,reg);
-		}
-		else
-		{
-         *(u16*)&aica_reg[addr]=(u16)data;
-			WriteChannelReg8(chan,reg);
-			WriteChannelReg8(chan,reg+1);
-		}
-		return;
-	}
-
 	if (addr<0x2800)
-	{
-		if (sz==1)
-         aica_reg[addr]=(u8)data;
-		else 
-         *(u16*)&aica_reg[addr]=(u16)data;
-		return;
-	}
+		ReadMemArrRet(aica_reg,addr,2);
 
 	if (addr < 0x2818)
-	{
-		if (sz==1)
-		{
-			WriteCommonReg8(addr,data);
-		}
-		else
-		{
-			WriteCommonReg8(addr,data&0xFF);
-			WriteCommonReg8(addr+1,data>>8);
-		}
-		return;
-	}
+   {
+      ReadCommonReg(addr,false);
+      return *(u16*)&aica_reg[addr];
+   }
 
-	if (addr>=0x3000)
-	{
-		if (sz==1)
-		{
-         aica_reg[addr]=(u8)data;
-			dsp_writenmem(addr);
-		}
-		else
-		{
-         *(u16*)&aica_reg[addr]=(u16)data;
-			dsp_writenmem(addr);
-			dsp_writenmem(addr+1);
-		}
-	}
-	if (sz==1)
-		WriteAicaReg<1>(addr,data);
-	else
-		WriteAicaReg<2>(addr,data);
+	ReadMemArrRet(aica_reg,addr,2);
 }
 
 /* Aica reads (both sh4&arm) */
 u32 libAICA_ReadReg(u32 addr,u32 size)
 {
    if (size==1)
-      return ReadReg<1>(addr & 0x7FFF);
-   return ReadReg<2>(addr & 0x7FFF);
+      return ReadReg1(addr & 0x7FFF);
+
+   return ReadReg2(addr & 0x7FFF);
+}
+
+static void WriteReg2(u32 addr,u32 data)
+{
+	if (addr < 0x2000)
+	{
+		//Channel data
+		u32 chan=addr>>7;
+		u32 reg=addr&0x7F;
+      *(u16*)&aica_reg[addr]=(u16)data;
+      WriteChannelReg8(chan,reg);
+      WriteChannelReg8(chan,reg+1);
+		return;
+	}
+
+	if (addr<0x2800)
+	{
+      *(u16*)&aica_reg[addr]=(u16)data;
+		return;
+	}
+
+	if (addr < 0x2818)
+	{
+      WriteCommonReg8(addr,data&0xFF);
+      WriteCommonReg8(addr+1,data>>8);
+		return;
+	}
+
+	if (addr>=0x3000)
+	{
+      *(u16*)&aica_reg[addr]=(u16)data;
+      dsp_writenmem(addr);
+      dsp_writenmem(addr+1);
+	}
+
+   WriteAicaReg<2>(addr,data);
+}
+
+static void WriteReg1(u32 addr,u32 data)
+{
+	if (addr < 0x2000)
+	{
+		//Channel data
+		u32 chan=addr>>7;
+		u32 reg=addr&0x7F;
+      aica_reg[addr]=(u8)data;
+      WriteChannelReg8(chan,reg);
+		return;
+	}
+
+	if (addr<0x2800)
+	{
+      aica_reg[addr]=(u8)data;
+		return;
+	}
+
+	if (addr < 0x2818)
+	{
+      WriteCommonReg8(addr,data);
+		return;
+	}
+
+	if (addr>=0x3000)
+	{
+      aica_reg[addr]=(u8)data;
+      dsp_writenmem(addr);
+	}
+
+   WriteAicaReg<1>(addr,data);
 }
 
 void libAICA_WriteReg(u32 addr,u32 data,u32 size)
 {
 	if (size==1)
-		WriteReg<1>(addr & 0x7FFF,data);
+		WriteReg1(addr & 0x7FFF,data);
 	else
-		WriteReg<2>(addr & 0x7FFF,data);
+		WriteReg2(addr & 0x7FFF,data);
 }
 
 //Map using _vmem .. yay

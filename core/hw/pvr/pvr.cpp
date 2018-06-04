a6e64732a55067c17c2f987b5414dc0bf38016e4
twinaphex <libretro@gmail.com>
2018-06-05 01:13:29 +0200

    Start splitting up code

--- core/hw/pvr/pvr.cpp
+++ core/hw/pvr/pvr.cpp
@@ -23,15 +23,8 @@ vector<vram_block*> VramLocks[VRAM_SIZE/PAGE_SIZE];
 //vram 32-64b
 VArray2 vram;
 
-extern bool pal_needs_update;
-extern u32 _pal_rev_256[4];
-extern u32 _pal_rev_16[64];
-extern u32 pal_rev_256[4];
-extern u32 pal_rev_16[64];
 
-bool fog_needs_update=true;
 
-u8 pvr_regs[pvr_RegSize];
 
 u32 in_vblank=0;
 u32 clc_pvr_scanline;
@@ -219,30 +212,10 @@ void libPvr_LockedBlockWrite (vram_block* block,u32 addr)
 	rend_text_invl(block);
 }
 
+
 void libPvr_Reset(bool Manual)
 {
-	ID                  = 0x17FD11DB;
-	REVISION            = 0x00000011;
-	SOFTRESET           = 0x00000007;
-	SPG_HBLANK_INT.full = 0x031D0000;
-	SPG_VBLANK_INT.full = 0x01500104;
-	FPU_PARAM_CFG       = 0x0007DF77;
-	HALF_OFFSET         = 0x00000007;
-	ISP_FEED_CFG        = 0x00402000;
-	SDRAM_REFRESH       = 0x00000020;
-	SDRAM_ARB_CFG       = 0x0000001F;
-	SDRAM_CFG           = 0x15F28997;
-	SPG_HBLANK.full     = 0x007E0345;
-	SPG_LOAD.full       = 0x01060359;
-	SPG_VBLANK.full     = 0x01500104;
-	SPG_WIDTH.full      = 0x07F1933F;
-	VO_CONTROL.full     = 0x00000108;
-	VO_STARTX.full      = 0x0000009D;
-	VO_STARTY.full      = 0x00000015;
-	SCALER_CTL.full     = 0x00000400;
-	FB_BURSTCTRL        = 0x00090639;
-	PT_ALPHA_REF        = 0x000000FF;
-
+   Regs_Reset(Manual);
 	pvr_reconfigure_spg();
 	//rend_reset(); //*TODO* wtf ?
 }
@@ -841,49 +814,75 @@ static void rend_start_render(void)
    }
 }
 
+extern bool pal_needs_update;
+bool fog_needs_update=true;
+extern u32 _pal_rev_256[4];
+extern u32 _pal_rev_16[64];
+extern u32 pal_rev_256[4];
+extern u32 pal_rev_16[64];
+
+u8 pvr_regs[pvr_RegSize];
+
+u32 pvr_ReadReg(u32 addr)
+{
+	return PvrReg(addr,u32);
+}
+
 void pvr_WriteReg(u32 paddr,u32 data)
 {
 	u32 addr=paddr&pvr_RegMask;
 
-   switch (addr)
+   if (addr == ID_addr)
+      return;//read only
+   if (addr==REVISION_addr)
+		return;//read only
+	if (addr==TA_YUV_TEX_CNT_addr)
+		return;//read only
+
+   if (addr==STARTRENDER_addr)
    {
-      case ID_addr: /* read-only */
-      case REVISION_addr:
-      case TA_YUV_TEX_CNT_addr:
-         return;
-      case STARTRENDER_addr:
-         //start render
-         rend_start_render();
-         return;
-      case TA_LIST_INIT_addr:
-         if (data>>31)
-         {
-            ta_vtx_ListInit();
-            data=0;
-         }
-         break;
-      case SOFTRESET_addr:
-         if (data!=0)
-         {
-            if (data & 1)
-               ta_vtx_SoftReset();
-            data=0;
-         }
-         break;
-      case TA_LIST_CONT_addr:
-         //a write of anything works ?
-         ta_vtx_ListCont();
-         break;
-      case FB_R_CTRL_addr:
-      case SPG_CONTROL_addr:
-      case SPG_LOAD_addr:
-         PvrReg(addr,u32)=data;
-         pvr_reconfigure_spg();
-         return;
-      case TA_YUV_TEX_BASE_addr:
-         YUV_init();
-         break;
+      //start render
+      rend_start_render();
+      return;
+   }
+
+   if (addr==TA_LIST_INIT_addr)
+   {
+      if (data>>31)
+      {
+         ta_vtx_ListInit();
+         data=0;
+      }
+   }
+
+   if (addr==SOFTRESET_addr)
+   {
+      if (data!=0)
+      {
+         if (data&1)
+            ta_vtx_SoftReset();
+         data=0;
+      }
+   }
+
+   if (addr==TA_LIST_CONT_addr)
+	{
+		//a write of anything works ?
+		ta_vtx_ListCont();
 	}
+
+   if (addr == FB_R_CTRL_addr || 
+		addr == SPG_CONTROL_addr || 
+		addr == SPG_LOAD_addr)
+	{
+		PvrReg(addr,u32)=data;
+		//CalculateSync();
+      pvr_reconfigure_spg();
+		return;
+	}
+
+   if (addr == TA_YUV_TEX_BASE_addr)
+		YUV_init();
 
 	if (addr>=PALETTE_RAM_START_addr)
 	{
@@ -903,6 +902,31 @@ void pvr_WriteReg(u32 paddr,u32 data)
 		fog_needs_update=true;
 
 	PvrReg(addr,u32)=data;
+}
+
+void Regs_Reset(bool Manual)
+{
+	ID                  = 0x17FD11DB;
+	REVISION            = 0x00000011;
+	SOFTRESET           = 0x00000007;
+	SPG_HBLANK_INT.full = 0x031D0000;
+	SPG_VBLANK_INT.full = 0x01500104;
+	FPU_PARAM_CFG       = 0x0007DF77;
+	HALF_OFFSET         = 0x00000007;
+	ISP_FEED_CFG        = 0x00402000;
+	SDRAM_REFRESH       = 0x00000020;
+	SDRAM_ARB_CFG       = 0x0000001F;
+	SDRAM_CFG           = 0x15F28997;
+	SPG_HBLANK.full     = 0x007E0345;
+	SPG_LOAD.full       = 0x01060359;
+	SPG_VBLANK.full     = 0x01500104;
+	SPG_WIDTH.full      = 0x07F1933F;
+	VO_CONTROL.full     = 0x00000108;
+	VO_STARTX.full      = 0x0000009D;
+	VO_STARTY.full      = 0x00000015;
+	SCALER_CTL.full     = 0x00000400;
+	FB_BURSTCTRL        = 0x00090639;
+	PT_ALPHA_REF        = 0x000000FF;
 }
 
 

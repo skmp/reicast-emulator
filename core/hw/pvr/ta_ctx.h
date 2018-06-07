aad60e81e29cda397d9b4b91485e2f80a95f346a
twinaphex <libretro@gmail.com>
2018-06-07 18:16:48 +0200

    Backport https://github.com/flyinghead/reicast-emulator/commit/718c341aa46121f3c3b305160810228986a06eb3 - Increase  TA context size (verts, idx)

--- core/hw/pvr/ta_ctx.h
+++ core/hw/pvr/ta_ctx.h
@@ -184,8 +184,8 @@ struct TA_context
 #endif
       tad.Reset((u8*)OS_aligned_malloc(32, 2*1024*1024));
 
-		rend.verts.InitBytes(1024*1024,&rend.Overrun, "verts"); //up to 1 mb of vtx data/frame = ~ 38k vtx/frame
-		rend.idx.Init(60*1024,&rend.Overrun, "idx");			//up to 60K indexes ( idx have stripification overhead )
+		rend.verts.InitBytes(4*1024*1024,&rend.Overrun, "verts"); //up to 4 mb of vtx data/frame = ~ 96k vtx/frame
+		rend.idx.Init(120*1024,&rend.Overrun, "idx");			//up to 120K indexes ( idx have stripification overhead )
 		rend.global_param_op.Init(4096,&rend.Overrun, "global_param_op");
 		rend.global_param_pt.Init(4096,&rend.Overrun, "global_param_pt");
 		rend.global_param_mvo.Init(4096,&rend.Overrun, "global_param_mvo");

03d582c10949110ce5e4f7aa45a31f8dce0b4bf0
twinaphex <libretro@gmail.com>
2018-07-12 23:45:53 +0200

    Added these entries

--- core/rom_luts.h
+++ core/rom_luts.h
@@ -50,6 +50,7 @@ static struct game_type lut_games[] =
    { "T47801M   ", -1, -1, -1, -1, -1, -1,   1, -1  },                /* Chaos Field (Japan) */
    { "T23202M   ", -1, -1, -1, -1, -1, -1,   1, -1  },                /* Death Crimson OX (Japan) */
    { "T2401N    ", -1, -1, -1, -1, -1, -1,   1, -1  },                /* Death Crimson OX (USA) */
+   { "T1223M    ", -1, -1, -1, -1, -1, -1,  	1, -1  },              	/* Eldorado Gate Vol. 1 (Japan) */
    { "T44306N   ", -1, -1, -1, -1, -1, -1,   1, -1  },                /* Fatal Fury: Mark of the Wolves (USA) */
    { "T3108M    ", -1, -1, -1, -1, -1, -1,   1, -1  },                /* Garou: Mark of the Wolves (Japan) */
    { "HDR-0078  ", -1, -1, -1, -1, -1, -1,   1, -1  },                /* Jet Set Radio (Japan) */
@@ -92,6 +93,7 @@ static struct game_type lut_games[] =
 
    /* Disable DIV matching */
    { "T30701D50 ", -1, -1, -1, -1, -1,  -1,  -1, 1  },                /* Pro Pinball Trilogy */
+   { "T15112N   ", -1, -1, -1, -1, -1,  -1,  -1, 1  },               /* Demolition Racer */
 };
 
 static struct game_type_naomi lut_games_naomi[] = 

35b7f00beabd5fe45e6864d9c8396d3f84149bf0
twinaphex <libretro@gmail.com>
2018-07-13 13:13:57 +0200

    Revert "Backport https://github.com/flyinghead/reicast-emulator/commit/ea35eeb72871996121f1b6474ee9c6e3b25dfa63"
    This reverts commit 874ea198f21d815173a5e9c9c3370161a6421842.

--- core/hw/sh4/interpr/sh4_fpu.cpp
+++ core/hw/sh4/interpr/sh4_fpu.cpp
@@ -628,25 +628,22 @@ sh4op(i1111_nnnn_0011_1101)
 	if (fpscr.PR == 0)
 	{
 		u32 n = GetN(op);
-      fpul = (u32)(s32)min(fr[n], 2147483520.0f);	// IEEE 754: 0x4effffff
+		fpul = (u32)(s32)min(fr[n],(float)0x7FFFFFBF);
 
-      // Intel CPUs convert out of range float numbers to 0x80000000. Manually set the correct sign
-      if (fpul == 0x80000000)
+		if (fpul==0x80000000) //this is actually a x86-specific fix. I think ARM saturates
 		{
-         if (*(int *)&fr[n] > 0)	// Using integer math to avoid issues with Inf and NaN
+			if (fr[n]>0)
 				fpul--;
 		}
 	}
 	else
 	{
 		u32 n = (op >> 9) & 0x07;
-      f64 f = GetDR(n);
-		fpul = (u32)(s32)f;
+		fpul = (u32)(s32)GetDR(n);
 
-      // Intel CPUs convert out of range float numbers to 0x80000000. Manually set the correct sign
-      if (fpul == 0x80000000)
+		if (fpul==0x80000000) //this is actually a x86-specific fix. I think ARM saturates
 		{
-         if (*(s64 *)&f > 0)	// Using integer math to avoid issues with Inf and NaN
+			if (GetDR(n)>0)
 				fpul--;
 		}
 	}

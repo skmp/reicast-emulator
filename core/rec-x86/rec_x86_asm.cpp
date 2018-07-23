d99018cb172f4414fb0404ed9c04e93c9589828d
twinaphex <libretro@gmail.com>
2018-07-23 21:44:33 +0200

    Backport flyinghead commits

--- core/rec-x86/rec_x86_asm.cpp
+++ core/rec-x86/rec_x86_asm.cpp
@@ -5,6 +5,9 @@
 #include "rec_x86_ngen.h"
 
 u32 gas_offs=offsetof(Sh4RCB,cntx.jdyn);
+
+const u32 cpurun_offset=offsetof(Sh4RCB,cntx.CpuRunning);
+
 void (*ngen_FailedToFindBlock)()=&ngen_FailedToFindBlock_;
 #endif
 

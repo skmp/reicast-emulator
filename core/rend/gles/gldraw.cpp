faab68b1abed462db7df76a68f57d97e8afb83c0
twinaphex <libretro@gmail.com>
2018-07-31 01:37:36 +0200

    Backport https://github.com/reicast/reicast-emulator/commit/153309115115ff65a1fe65ac08f8f46ed95dbb17

--- core/rend/gles/gldraw.cpp
+++ core/rend/gles/gldraw.cpp
@@ -106,11 +106,12 @@ s32 SetTileClip(u32 val, bool set)
 	
 	if (set && clip_mode)
    {
-      csx /= scale_x;
-      csy /= scale_y;
-      cex /= scale_x;
-      cey /= scale_y;
-      if (!pvrrc.isRTT) {
+      if (!pvrrc.isRTT)
+      {
+         csx /= scale_x;
+         csy /= scale_y;
+         cex /= scale_x;
+         cey /= scale_y;
          float t = cey;
          cey = 480 - csy;
          csy = 480 - t;

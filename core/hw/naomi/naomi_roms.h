e4c43a76b8d5ecb3b45287a078c289697379acab
barbudreadmon <barbudreadmon@gmail.com>
2019-04-11 13:55:08 +0200

    Implement some eeproms (vertical, pstone2, alpilot)

--- core/hw/naomi/naomi_roms.h
+++ core/hw/naomi/naomi_roms.h
@@ -809,7 +809,8 @@ Games[] =
             { NULL, 0, 0 },
         },
         NULL,
-        &alpilot_inputs
+        &alpilot_inputs,
+        alpilot_eeprom_dump
     },
     // Airline Pilots (Japan, Rev A)
     {
@@ -837,7 +838,8 @@ Games[] =
             { NULL, 0, 0 },
         },
         NULL,
-        &alpilot_inputs
+        &alpilot_inputs,
+        alpilot_eeprom_dump
     },
     // Alien Front (Rev T)
     {
@@ -2491,7 +2493,10 @@ Games[] =
             { "mpr-23125.ic8",  0x4000000, 0x0800000 },
             { "mpr-23126.ic9",  0x4800000, 0x0800000 },
             { NULL, 0, 0 },
-        }
+        },
+        NULL,
+        NULL,
+        pstone2_eeprom_dump
     },
     // Power Stone 2 (bootleg)
     {
@@ -2515,7 +2520,10 @@ Games[] =
             { "08.ic9",  0x4000000, 0x0800000 },
             { "09.ic10", 0x4800000, 0x0800000 },
             { NULL, 0, 0 },
-        }
+        },
+        NULL,
+        NULL,
+        pstone2_eeprom_dump
     },
     // Puyo Puyo Da! (Japan)
     {
@@ -3983,7 +3991,10 @@ Games[] =
             { "fpr-24439.ic10", 0x8000000, 0x4000000 },
             { "317-5131-jpn.ic3", 0, 0x800, Key },
             { NULL, 0, 0 },
-        }
+        },
+        NULL,
+        NULL,
+        illvelo_eeprom_dump
     },
     // Manic Panic Ghosts! *** BAD DUMP ***
     {
@@ -4217,7 +4228,10 @@ Games[] =
             { "fpr-24416.ic11", 0xc000000, 0x4000000 },
             { "317-5129-jpn.ic3", 0, 0x800, Key },
             { NULL, 0, 0 },
-        }
+        },
+        NULL,
+        NULL,
+        sl2007_eeprom_dump
     },
     // Touch De Zunou (Rev A) *** BAD DUMP ***
     {

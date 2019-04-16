9e6ffdb58f993d1f8b6de3ab118eba0e9a33ea28
Dave Diffenbaugh <daviddi@microsoft.com>
2019-04-15 23:39:22 -0700

    cleanup

--- core/osd/uwp/reicastApp.cpp
+++ core/osd/uwp/reicastApp.cpp
@@ -8,65 +8,16 @@
 #include "types.h"
 
 using namespace winrt;
+using namespace Windows;
+using namespace Windows::ApplicationModel::Core;
 using namespace Windows::Foundation;
+using namespace Windows::UI;
+using namespace Windows::UI::Core;
 
 u16 kcode[4];
 u32 vks[4];
 s8 joyx[4], joyy[4];
 u8 rt[4], lt[4];
-
-int get_mic_data(u8* buffer) { return 0; }
-int push_vmu_screen(u8* buffer) { return 0; }
-
-void os_DebugBreak() {}
-
-
-void os_SetWindowText(const char * text) {
-	puts(text);
-}
-
-void os_DoEvents() {
-
-}
-
-void os_SetupInput() {
-	//mcfg_CreateDevicesFromConfig();
-}
-
-//void os_DebugBreak() { return; } // SIGNAL()
-
-
-void UpdateInputState(u32 port) {
-
-}
-
-void UpdateVibration(u32 port, u32 value) {
-
-}
-
-void os_CreateWindow() { }
-
-
-int __cdecl msgboxf(const wchar_t* text, unsigned int type, ...)
-{
-	va_list args;
-
-	wchar_t temp[2048];
-	va_start(args, type);
-	vswprintf(temp, text, args);
-	va_end(args);
-
-	wprintf(L"----------------------------------------------\n");
-	wprintf(L"msgbox(\" %s \" ); \n", temp);
-	wprintf(L"----------------------------------------------\n\n");
-//	QMessageBox::information(0, "", temp, QMessageBox::Ok);
-	return 0;
-}
-
-using namespace Windows;
-using namespace Windows::ApplicationModel::Core;
-using namespace Windows::UI;
-using namespace Windows::UI::Core;
 
 struct App : implements<App, IFrameworkViewSource, IFrameworkView>
 {
@@ -105,4 +56,45 @@ struct App : implements<App, IFrameworkViewSource, IFrameworkView>
 int __stdcall main(HINSTANCE, HINSTANCE, PWSTR, int)
 {
 	CoreApplication::Run(make<App>());
+}
+
+int get_mic_data(u8* buffer) { return 0; }
+int push_vmu_screen(u8* buffer) { return 0; }
+
+void os_DebugBreak() {
+}
+
+void os_SetWindowText(const char * text) {
+	puts(text);
+}
+
+void os_DoEvents() {
+}
+
+void os_SetupInput() {
+}
+
+void UpdateInputState(u32 port) {
+}
+
+void UpdateVibration(u32 port, u32 value) {
+}
+
+void os_CreateWindow() {
+}
+
+int __cdecl msgboxf(const wchar_t* text, unsigned int type, ...)
+{
+	va_list args;
+
+	wchar_t temp[2048];
+	va_start(args, type);
+	vswprintf(temp, text, args);
+	va_end(args);
+
+	wprintf(L"----------------------------------------------\n");
+	wprintf(L"msgbox(\" %s \" ); \n", temp);
+	wprintf(L"----------------------------------------------\n\n");
+	//	QMessageBox::information(0, "", temp, QMessageBox::Ok);
+	return 0;
 }

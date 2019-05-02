ffd7bdb8356b24ccec64993822b69513276c7915
Flyinghead <raphael.jean@gmail.com>
2019-05-02 11:49:12 +0200

    missing files from previous commit

---
+++ core/hw/mem/vmem32.h
@@ -0,0 +1,15 @@
+#include "types.h"
+
+bool vmem32_init();
+void vmem32_term();
+bool vmem32_handle_signal(void *fault_addr, bool write);
+void vmem32_flush_mmu();
+void vmem32_protect_vram(vram_block *block);
+void vmem32_unprotect_vram(vram_block *block);
+static inline bool vmem32_enabled() {
+#if 1 //HOST_OS == OS_WINDOWS
+	return false;
+#else
+	return !settings.dynarec.disable_vmem32;
+#endif
+}

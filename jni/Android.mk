69a2e6446f6ec9b5d5dd8e2da32eaf69b87c4b73
Aaron Kling <webgeek1234@gmail.com>
2019-06-07 11:11:36 -0500

    Fix android x86 compile

--- jni/Android.mk
+++ jni/Android.mk
@@ -61,6 +61,11 @@ endif
 ifeq ($(TARGET_ARCH_ABI), x86)
 	COREFLAGS += -DHOST_CPU=$(HOST_CPU_X86)
 endif
+ifeq ($(WITH_DYNAREC), x86)
+	# X86 dynarec isn't position independent, so it fails to build on newer ndks.
+	# No warn shared textrel allows it to build, but still won't allow it to run on api 23+.
+	CORELDLIBS := -Wl,-no-warn-shared-textrel
+endif
 
 ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
 	COREFLAGS += -DHOST_CPU=$(HOST_CPU_ARM)
@@ -89,7 +94,7 @@ LOCAL_SRC_FILES    := $(SOURCES_CXX) $(SOURCES_C) $(SOURCES_ASM)
 LOCAL_CFLAGS       := $(COREFLAGS) $(CFLAGS)
 LOCAL_CXXFLAGS     := -std=c++11 $(COREFLAGS) $(CXXFLAGS)
 LOCAL_LDFLAGS      := -Wl,-version-script=$(CORE_DIR)/link.T
-LOCAL_LDLIBS       := -lGLESv2 -llog
+LOCAL_LDLIBS       := -lGLESv2 -llog $(CORELDLIBS)
 LOCAL_CPP_FEATURES := exceptions
 LOCAL_ARM_NEON     := true
 LOCAL_ARM_MODE     := arm

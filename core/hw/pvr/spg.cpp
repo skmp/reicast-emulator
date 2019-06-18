cf4dda6f5361ab30cc9cd4915b0e2b94ed5888de
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2019-06-18 13:00:44 +0300

    Stats: Use os_SetWindowText instead of console print

--- core/hw/pvr/spg.cpp
+++ core/hw/pvr/spg.cpp
@@ -186,12 +186,15 @@ int spg_line_sched(int tag, int cycl, int jit)
 					mode,res,fullvbs,
 					spd_fps,fskip/ts);
 				#else
-				printf("%s/%c - %4.2f - %4.2f - V: %4.2f (%.2f, %s%s%4.2f) R: %4.2f+%4.2f VTX: %4.2f%c, MIPS: %.2f\n",
+				char temp[2048];
+				snprintf(temp, 2048, "%s/%c - %4.2f - %4.2f - V: %4.2f (%.2f, %s%s%4.2f) R: %4.2f+%4.2f VTX: %4.2f%c, MIPS: %.2f",
 					VER_SHORTNAME,'n',mspdf,spd_cpu*100/200,spd_vbs,
 					spd_vbs/full_rps,mode,res,fullvbs,
 					spd_fps,fskip/ts
 					, mv, mv_c, mips_counter/ 1024.0 / 1024.0);
 					mips_counter = 0;
+
+				os_SetWindowText(temp);
 				#endif
 				
 				fskip=0;

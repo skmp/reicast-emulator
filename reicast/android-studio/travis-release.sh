3fa71dadf5dc7dc971e492450a7c2cc72189a2f6
Stefanos Kornilios Mitsis Poiitidis <skmp@emudev.org>
2019-07-12 10:41:39 +0200

    Travis: Release from `beta` branch

--- reicast/android-studio/travis-release.sh
+++ reicast/android-studio/travis-release.sh
@@ -2,7 +2,7 @@
 set -ev
 echo travis release: pr = "${TRAVIS_PULL_REQUEST}" - branch = "${TRAVIS_BRANCH}"
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  if [ "${TRAVIS_BRANCH}" = "master" ]; then
+  if [ "${TRAVIS_BRANCH}" = "beta" ]; then
     ./gradlew publishApkDreamcastRelease
   fi
 fi

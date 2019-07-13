dd18eaa1f029831058564f7a10b07bc3c6ebaca8
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2019-07-13 14:31:43 +0200

    Update travis for alpha/beta/stable

--- reicast/android-studio/travis-release.sh
+++ reicast/android-studio/travis-release.sh
@@ -2,7 +2,13 @@
 set -ev
 echo travis release: pr = "${TRAVIS_PULL_REQUEST}" - branch = "${TRAVIS_BRANCH}"
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+  if [ "${TRAVIS_BRANCH}" = "alpha" ]; then
+    ./gradlew publishApkDreamcastAlphaRelease
+  fi
   if [ "${TRAVIS_BRANCH}" = "beta" ]; then
-    ./gradlew publishApkDreamcastRelease
+    ./gradlew publishApkDreamcastBetaRelease
+  fi
+  if [ "${TRAVIS_BRANCH}" = "stable" ]; then
+    ./gradlew publishApkDreamcastStableRelease
   fi
 fi

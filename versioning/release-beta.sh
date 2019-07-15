fbb1cc3b545bcd5db8f3e29d57e1be116c8531a6
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2019-07-15 05:02:39 +0200

    scripts: fix release-beta.sh

--- versioning/release-beta.sh
+++ versioning/release-beta.sh
@@ -1,6 +1,6 @@
 echo "Make sure repo is up to date"
 git checkout origin/alpha
-git tag -a $1 -m "Reicast $1\n---\n`./changelog-beta.sh`"
+git tag -a $1 -m `echo "Reicast $1" && echo "---" && ./changelog-beta.sh`
 git checkout beta
 git reset --hard origin/alpha
 echo "You need to do:"

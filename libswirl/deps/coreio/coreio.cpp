372e4914bb37076beac7d17ff598a73e77764828
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2019-07-20 22:26:23 +0200

    refactor: make coreio more generic

--- libswirl/deps/coreio/coreio.cpp
+++ libswirl/deps/coreio/coreio.cpp
@@ -126,7 +126,7 @@ size_t HTTP_GET(string host, int port,string path, size_t offs, size_t len, void
 	/*
 		parse headers ..
 	*/
-	size_t content_length = 0;
+	int content_length = 0;
 	
 	size_t rv = 0;
 
@@ -193,24 +193,74 @@ _data:
 }
 #endif
 
-struct CORE_FILE {
-	FILE* f;
-	string path;
-	size_t seek_ptr;
+struct CoreFile
+{
+	size_t seek_ptr = 0;
 
-	string host;
-	int port;
+	virtual void seek() = 0;
+	virtual size_t read(void* buff, size_t len) = 0;
+	virtual size_t size() = 0;
+
+	virtual ~CoreFile() { }
 };
 
-core_file* core_fopen(const char* filename)
+struct CoreFileLocal: CoreFile
 {
-	string p = filename;
+	FILE* f = nullptr;
 
-	CORE_FILE* rv = new CORE_FILE();
-	rv->f = 0;
-	rv->path = p;
-#if FEAT_HAS_COREIO_HTTP
-	if (p.substr(0,7)=="http://") {
+	static CoreFile* open(const char* path)
+	{
+		FILE* f = fopen(path, "rb");
+
+		if (!f)
+			return nullptr;
+
+		CoreFileLocal* rv = new CoreFileLocal();
+
+		rv->f = f;
+
+		return rv;
+	}
+
+	size_t read(void* buff, size_t len)
+	{
+		return fread(buff,1,len,f);
+	}
+
+	void seek()
+	{
+		fseek(f, seek_ptr, SEEK_SET);
+	}
+
+	size_t size()
+	{
+		size_t p = ftell(f);
+		fseek(f, 0, SEEK_END);
+
+		size_t rv = ftell(f);
+		fseek(f, p, SEEK_SET);
+
+		return rv;
+	}
+
+	~CoreFileLocal() { fclose(f); }
+};
+
+struct CoreFileHTTP: CoreFile
+{
+	string path;
+	string host;
+	int port;
+
+	static CoreFile* open(const char* path)
+	{
+		string p = path;
+
+		if (p.substr(0,7)!="http://")
+			return nullptr;
+
+		CoreFileHTTP* rv = new CoreFileHTTP();
+
 		rv->host = p.substr(7,p.npos);
 		rv->host = rv->host.substr(0, rv->host.find_first_of("/"));
 
@@ -223,23 +273,58 @@ core_file* core_fopen(const char* filename)
 			rv->host = rv->host.substr(0, rv->host.find_first_of(":"));
 			sscanf(port.c_str(),"%d",&rv->port);
 		}
-	} else
-#endif	
-  {
-		rv->f = fopen(filename, "rb");
 
-		if (!rv->f) {
-			delete rv;
-			return 0;
-		}
+		return rv;
 	}
 
-	core_fseek((core_file*)rv, 0, SEEK_SET);
-	return (core_file*)rv;
+	size_t read(void* buff, size_t len)
+	{
+		return HTTP_GET(host, port, path, seek_ptr, len, buff);
+	}
+
+	void seek() { }
+
+	size_t size()
+	{
+		return HTTP_GET(host, port, path, 0, 0,0);
+	}
+
+	~CoreFileHTTP() { }
+};
+
+static CoreFile* (*vfs[])(const char* path) =
+{
+	&CoreFileHTTP::open,
+	&CoreFileLocal::open,
+	nullptr
+};
+
+core_file* core_fopen(const char* filename)
+{
+	CoreFile* rv;
+	for (int i = 0; vfs[i] != nullptr; i++)
+	{
+		auto fs_open = vfs[i];
+
+		rv = fs_open(filename);
+		if (rv)
+			break;
+	}
+
+	if (rv)
+	{
+		core_fseek((core_file*)rv, 0, SEEK_SET);
+		return (core_file*)rv;
+	}
+	else
+	{
+		printf("CoreIO: Failed to open %s\n", filename);
+		return nullptr;
+	}
 }
 
 size_t core_fseek(core_file* fc, size_t offs, size_t origin) {
-	CORE_FILE* f = (CORE_FILE*)fc;
+	CoreFile* f = (CoreFile*)fc;
 	
 	if (origin == SEEK_SET)
 		f->seek_ptr = offs;
@@ -248,40 +333,30 @@ size_t core_fseek(core_file* fc, size_t offs, size_t origin) {
 	else
 		die("Invalid code path");
 
-	if (f->f)
-		fseek(f->f, f->seek_ptr, SEEK_SET);
+	f->seek();
 
 	return 0;
 }
 
 size_t core_ftell(core_file* fc) {
-	CORE_FILE* f = (CORE_FILE*)fc;
+	CoreFile* f = (CoreFile*)fc;
 	return f->seek_ptr;
 }
+
 int core_fread(core_file* fc, void* buff, size_t len)
 {
-	CORE_FILE* f = (CORE_FILE*)fc;
+	CoreFile* f = (CoreFile*)fc;
 
-	if (f->f) {
-		fread(buff,1,len,f->f);
-	} else {
-	#if FEAT_HAS_COREIO_HTTP
-		HTTP_GET(f->host, f->port, f->path, f->seek_ptr, len, buff);
-	#endif
-	}
+	auto rv = f->read(buff, len);
 
-	f->seek_ptr += len;
+	f->seek_ptr += rv;
 
-	return len;
+	return rv;
 }
 
 int core_fclose(core_file* fc)
 {
-	CORE_FILE* f = (CORE_FILE*)fc;
-
-	if (f->f) {
-		fclose(f->f);
-	}
+	CoreFile* f = (CoreFile*)fc;
 
 	delete f;
 
@@ -290,19 +365,7 @@ int core_fclose(core_file* fc)
 
 size_t core_fsize(core_file* fc)
 {
-	CORE_FILE* f = (CORE_FILE*)fc;
+	CoreFile* f = (CoreFile*)fc;
 
-	if (f->f) {
-		size_t p=ftell(f->f);
-		fseek(f->f,0,SEEK_END);
-		size_t rv=ftell(f->f);
-		fseek(f->f,p,SEEK_SET);
-		return rv;
-	}
-	else {
-	#if FEAT_HAS_COREIO_HTTP
-		return HTTP_GET(f->host, f->port, f->path, 0, 0,0);
-	#endif
-	}
-    return 0;
+    return f->size();
 }

a0b41ac0559764d932ea1ad4bff87a4d8fb9a7ed
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2020-02-01 09:52:03 +0200

    Android: Remove View.SYSTEM_UI_FLAG_LAYOUT_STABLE, move IMMESRIVE logic to activity

--- reicast/android-studio/reicast/src/main/java/com/reicast/emulator/NativeGLActivity.java
+++ reicast/android-studio/reicast/src/main/java/com/reicast/emulator/NativeGLActivity.java
@@ -1,12 +1,15 @@
 package com.reicast.emulator;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.RelativeLayout;
@@ -119,5 +122,21 @@ public final class NativeGLActivity extends BaseGLActivity {
         }
 
         return 1;
+    }
+
+    @TargetApi(19)
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus && Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            /* not using SYSTEM_UI_FLAG_LAYOUT_STABLE  as it seems to be buggy on Pixel3a ~ skmp*/
+        }
     }
 }

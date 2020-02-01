8678abea4b0e701ee6ba7600c0d99fb35d18d17e
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2020-02-01 09:52:03 +0200

    Android: Remove View.SYSTEM_UI_FLAG_LAYOUT_STABLE, move IMMESRIVE logic to activity

--- reicast/android-studio/reicast/src/main/java/com/reicast/emulator/NativeGLActivity.java
+++ reicast/android-studio/reicast/src/main/java/com/reicast/emulator/NativeGLActivity.java
@@ -1,10 +1,13 @@
 package com.reicast.emulator;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.RelativeLayout;
@@ -85,5 +88,21 @@ public final class NativeGLActivity extends BaseGLActivity {
                 mLayout.addView(mView);
             }
         });
+    }
+
+    @TargetApi(19)
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus && Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            /* not using SYSTEM_UI_FLAG_LAYOUT_STABLE  as it seems to be buggy on Pixel3a ~ skmp*/
+        }
     }
 }
--- reicast/android-studio/reicast/src/main/java/com/reicast/emulator/emu/NativeGLView.java
+++ reicast/android-studio/reicast/src/main/java/com/reicast/emulator/emu/NativeGLView.java
@@ -120,22 +120,6 @@ public class NativeGLView extends SurfaceView implements SurfaceHolder.Callback 
         }
     }
 
-    @TargetApi(19)
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus && Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-            requestLayout();
-        }
-    }
-
     public void readCustomVjoyValues() {
         vjoyDelegate.readCustomVjoyValues();
     }

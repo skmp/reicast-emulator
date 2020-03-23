35e90950b5a533507385a49b6f6883e28bddd2d6
Ben Baron <ben@einsteinx2.com>
2020-03-22 22:32:59 -0500

    DSP: removed unnecessary flags from build_macos.sh

--- aica-dsp-asm/build_macos.sh
+++ aica-dsp-asm/build_macos.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-clang++ -target x86_64-apple-macos10.9 -std=c++14 -stdlib=libc++ main.cpp macos_support.mm ../libswirl/hw/aica/dsp_helpers.cpp -I../libswirl -DTARGET_OSX_X64 -DHAVE_STDLIB_H -std=c++11 -framework Foundation -o aica-dsp-asm
+clang++ -std=c++14 -stdlib=libc++ main.cpp macos_support.mm ../libswirl/hw/aica/dsp_helpers.cpp -I../libswirl -DTARGET_OSX_X64 -framework Foundation -o aica-dsp-asm

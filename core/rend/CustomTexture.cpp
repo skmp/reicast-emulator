2db065b7ae3a5bef748cfb7999255babcd64733a
twinaphex <libretro@gmail.com>
2021-03-03 17:18:22 +0100

    Backport https://github.com/flyinghead/flycast/commit/ac7bd2a6a076bd083b65b57d92b8543c8d1f4c08

--- core/rend/CustomTexture.cpp
+++ core/rend/CustomTexture.cpp
@@ -61,7 +61,7 @@ void CustomTexture::LoaderThread()
 				texture->ComputeHash();
 				if (texture->custom_image_data != NULL)
 				{
-					delete [] texture->custom_image_data;
+					free(texture->custom_image_data);
 					texture->custom_image_data = NULL;
 				}
 				if (!texture->dirty)

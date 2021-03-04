b210cdeff942d894f7db356005f34736e3fb3ca5
twinaphex <libretro@gmail.com>
2021-03-04 23:51:44 +0100

    Backport https://github.com/flyinghead/flycast/commit/3f1326a3ac12f816a89b9aa7e2aacdc1809d55d6

--- core/hw/pvr/pvr_regs.cpp
+++ core/hw/pvr/pvr_regs.cpp
@@ -98,6 +98,10 @@ void pvr_WriteReg(u32 paddr,u32 data)
 		}
 		break;
 
+   case FB_R_SOF2_addr:
+      data &= 0x00fffffc;
+      break;
+
 	case FB_W_SOF1_addr:
 	case FB_W_SOF2_addr:
 		data &= 0x01fffffc;

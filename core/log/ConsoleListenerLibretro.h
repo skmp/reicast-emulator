7eaf1e5fc3f4d6d71c759c69c20b90a5c45b6f35
twinaphex <libretro@gmail.com>
2021-03-06 04:58:41 +0100

    Refactor LogManager code

--- core/log/ConsoleListenerLibretro.h
+++ core/log/ConsoleListenerLibretro.h
@@ -4,7 +4,7 @@
 
 #pragma once
 
-#include "LogManager.h"
+#include "LogManagerLibretro.h"
 #include <libretro.h>
 
 class ConsoleListener : public LogListener
--- core/log/LogManagerLibretro.cpp
+++ core/log/LogManagerLibretro.cpp
@@ -2,7 +2,7 @@
 // Licensed under GPLv2+
 // Refer to the license.txt file included.
 
-#include "LogManager.h"
+#include "LogManagerLibretro.h"
 
 #include <algorithm>
 #include <cstdarg>

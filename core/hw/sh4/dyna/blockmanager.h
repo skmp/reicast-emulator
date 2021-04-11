5c3c881c7fd321265c23a40f05fd0246ad7f4bdb
twinaphex <libretro@gmail.com>
2021-04-11 04:26:37 +0200

    Backport https://github.com/flyinghead/flycast/commit/fe17d79e8da08692db31dd9b6610740b0f334a66

--- core/hw/sh4/dyna/blockmanager.h
+++ core/hw/sh4/dyna/blockmanager.h
@@ -77,8 +77,8 @@ struct RuntimeBlockInfo: RuntimeBlockInfo_Core
 	//predecessors references
 	vector<RuntimeBlockInfoPtr> pre_refs;
 
-	void AddRef(RuntimeBlockInfoPtr other);
-	void RemRef(RuntimeBlockInfoPtr other);
+   void AddRef(const RuntimeBlockInfoPtr& other);
+	void RemRef(const RuntimeBlockInfoPtr& other);
 
 	void Discard();
 	void UpdateRefs();
